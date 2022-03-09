f9f328d000b5aeacc3c1afc299d50369ea6f27fd
Gunvir Ranu <mail@gunvirranu.com>
2022-03-09 08:58:34 -0800

    Impl SGP4 error code enum type

--- include/perturb/perturb.hpp
+++ include/perturb/perturb.hpp
@@ -3,6 +3,17 @@
 
 namespace perturb {
 
+enum class Sgp4Error : int {
+    NONE = 0,
+    MEAN_ELEMENTS,
+    MEAN_MOTION,
+    PERT_ELEMENTS,
+    SEMI_LATUS_RECTUM,
+    EPOCH_ELEMENTS_SUB_ORBITAL,
+    DECAYED,
+    UNKNOWN
+};
+
 }  // namespace perturb
 
 #endif  // PERTURB_PERTURB_HPP
--- src/perturb.cpp
+++ src/perturb.cpp
@@ -2,4 +2,11 @@
 
 namespace perturb {
 
+Sgp4Error convert_sgp4_error_code(const int error_code) {
+    if (error_code < 0 || error_code >= static_cast<int>(Sgp4Error::UNKNOWN)) {
+        return Sgp4Error::UNKNOWN;
+    }
+    return static_cast<Sgp4Error>(error_code);
+}
+
 }  // namespace perturb
