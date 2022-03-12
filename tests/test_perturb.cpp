8756bfc4ecb88c592177e10d4f7cba2a042799de
Gunvir Ranu <mail@gunvirranu.com>
2022-03-11 16:21:37 -0800

    Add tests infra using doctest

---
+++ tests/test_perturb.cpp
@@ -0,0 +1,10 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+
+#include "perturb/perturb.hpp"
+
+using namespace::perturb;
+
+TEST_CASE("test_something") {
+    CHECK(1 == 1);
+}
