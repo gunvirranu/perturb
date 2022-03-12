16fadd534828ed14e9f903dcf24255ab08e7b3a5
Gunvir Ranu <mail@gunvirranu.com>
2022-03-11 18:49:51 -0800

    Test julian date conversions from 1901 to 2099
    Somehow, I didn't even need to add epsilon checks for comparing JDs.
    The floating point vals are literally equal already. Makes me a bit
    suspicious about the precision. Maybe I do need jd_frac...

--- tests/test_perturb.cpp
+++ tests/test_perturb.cpp
@@ -5,6 +5,21 @@
 
 using namespace::perturb;
 
-TEST_CASE("test_something") {
-    CHECK(1 == 1);
+TEST_CASE(
+    "test_julian_date"
+    * doctest::description("Check julian date conversions from 1901 to 2099")
+) {
+    constexpr double JD_START = 2415750.5;
+    constexpr double JD_END = 2488068.5;
+    constexpr std::size_t N_CHECKS = 123479;  // Shouldn't evenly divide range
+    constexpr double DELTA_JD = (JD_END - JD_START) / N_CHECKS;
+
+    for (std::size_t i = 0; i < N_CHECKS; ++i) {
+        const auto jd = JulianDate(JD_START + i * DELTA_JD);
+        const YMDhms t = jd.to_datetime();
+        CHECK_MESSAGE(
+            jd.jd == JulianDate(t).jd,
+            t.year, "-", t.month, "-", t.day, " ", t.hour, ":", t.min, ":", t.sec
+        );
+    }
 }
