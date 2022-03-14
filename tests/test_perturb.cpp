07c9351e20d478af6b742e53b624bfde80205568
Gunvir Ranu <mail@gunvirranu.com>
2022-03-14 02:23:42 -0700

    Test verification mode from Vallado's source

--- tests/test_perturb.cpp
+++ tests/test_perturb.cpp
@@ -2,17 +2,38 @@
 #include "doctest/doctest.h"
 
 #include <array>
-#include <cstring>
+#include <cstdio>
 #include <cmath>
+#include <cstring>
+#include <fstream>
 
 #include "perturb/perturb.hpp"
 
 using namespace perturb;
 
+constexpr double PI = 3.14159265358979323846;
+constexpr double RAD_TO_DEG = 180 / PI;
+constexpr double DEG_TO_RAD = PI / 180;
+
 std::array<char, TLE_LINE_LEN> str_to_arr(const char *str) {
     std::array<char, TLE_LINE_LEN> line;
     std::memcpy(line.data(), str, TLE_LINE_LEN);
     return line;
+}
+
+Satellite sat_from_verif_tle(
+    std::string &line_1, std::string &line_2,
+    double &startmfe, double &stopmfe, double &deltamin
+) {
+    constexpr char OPS_MODE = 'a';
+    REQUIRE(line_1.length() >= TLE_LINE_LEN);
+    REQUIRE(line_2.length() >= TLE_LINE_LEN);
+    vallado_sgp4::elsetrec sat_rec;
+    vallado_sgp4::twoline2rv(
+        &line_1[0], &line_2[0], 'v', 'e', OPS_MODE,
+        vallado_sgp4::wgs72, startmfe, stopmfe, deltamin, sat_rec
+    );
+    return Satellite(sat_rec);
 }
 
 double norm(const Vec3 &v) {
@@ -135,4 +156,134 @@ TEST_CASE("test_sgp4_iss_tle") {
             CHECK(vel_1[2] == doctest::Approx(-vel_2[2]).epsilon(EPS));
         }
     }
+}
+
+TEST_CASE(
+    "test_sgp4_verification_mode"
+    // Can't run verification mode without debug mode
+    * doctest::skip(!PERTURB_VALLADO_SGP4_ENABLE_DEBUG)
+    * doctest::description("Run verification mode based off Vallado's test code")
+) {
+    std::ifstream in_file("SGP4-VER.TLE");
+    REQUIRE(in_file.is_open());
+    FILE *out_file = std::fopen("generated-tcppver.out", "w");
+    REQUIRE(out_file != nullptr);
+
+    Vec3 pos, vel;
+    std::string line_1, line_2;
+    while (std::getline(in_file, line_1)) {
+        if (line_1[0] == '#') {
+            continue;
+        }
+        REQUIRE(std::getline(in_file, line_2));
+
+        double startmfe, stopmfe, deltamin;
+        auto sat = sat_from_verif_tle(line_1, line_2, startmfe, stopmfe, deltamin);
+
+        sat.propogate_from_epoch(0.0, pos, vel);  // Initialize maybe??
+        std::fprintf(out_file, "%s xx\n", sat.sat_rec.satnum);
+        std::fprintf(
+            out_file,
+            " %16.8f %16.8f %16.8f %16.8f %12.9f %12.9f %12.9f\n",
+            sat.sat_rec.t, pos[0], pos[1], pos[2], vel[0], vel[1], vel[2]
+        );
+
+        double tsince = startmfe;
+        if (std::fabs(tsince) > 1e-8) {
+            tsince -= deltamin;
+        }
+
+        while ((tsince < stopmfe) && sat.last_error() == Sgp4Error::NONE) {
+            tsince = std::min(tsince + deltamin, stopmfe);
+            if (sat.propogate_from_epoch(tsince, pos, vel) != Sgp4Error::NONE) {
+                continue;
+            }
+
+            // FIXME: Change to this when fractional JD added to `JulianDate`
+            // const auto jd = sat.epoch() + tsince / 1440.0;
+            // const auto ymdhms = jd.to_datetime();
+
+            int year, mon, day, hr, min;
+            double sec;
+            double jd = sat.sat_rec.jdsatepoch;
+            double jd_frac = sat.sat_rec.jdsatepochF + tsince / 1440.0;
+            if (jd_frac < 0) {
+                jd -= 1;
+                jd_frac += 1;
+            }
+            vallado_sgp4::invjday_SGP4(jd, jd_frac, year, mon, day, hr, min, sec);
+            const YMDhms ymdhms { year, mon, day, hr, min, sec };
+
+            std::fprintf(
+                out_file,
+                " %16.8f %16.8f %16.8f %16.8f %12.9f %12.9f %12.9f",
+                tsince, pos[0], pos[1], pos[2], vel[0], vel[1], vel[2]
+            );
+
+            double p, a, ecc, incl, node, argp, nu, m, arglat, truelon, lonper;
+            vallado_sgp4::rv2coe_SGP4(
+                pos.data(), vel.data(), sat.sat_rec.mus,
+                p, a, ecc, incl, node, argp, nu, m, arglat, truelon, lonper
+            );
+            std::fprintf(
+                out_file,
+                " %14.6f %8.6f %10.5f %10.5f %10.5f %10.5f %10.5f %5i%3i%3i %2i:%2i:%9.6f\n",
+                a, ecc, incl * RAD_TO_DEG, node * RAD_TO_DEG, argp * RAD_TO_DEG, nu * RAD_TO_DEG, m * RAD_TO_DEG,
+                ymdhms.year, ymdhms.month, ymdhms.day, ymdhms.hour, ymdhms.min, ymdhms.sec
+            );
+        }
+    }
+
+    // Useless scope so it can be visually folded
+    {
+        vallado_sgp4::elsetrec sat_rec {};
+        std::strcpy(sat_rec.satnum, "8195");
+        sat_rec.jdsatepoch = 2453911.0;
+        sat_rec.jdsatepochF = 0.8321544402;
+        sat_rec.no_kozai = 2.00491383;
+        sat_rec.ecco = 0.6877146;
+        sat_rec.inclo = 64.1586;
+        sat_rec.nodeo = 279.0717;
+        sat_rec.argpo = 264.7651;
+        sat_rec.mo = 20.2257;
+        sat_rec.nddot = 0.00000e0;
+        sat_rec.bstar = 0.11873e-3;
+        sat_rec.ndot = 0.00000099;
+        sat_rec.elnum = 813;
+        sat_rec.revnum = 22565;
+        sat_rec.classification = 'U';
+        sat_rec.ephtype = 0;
+        std::strcpy(sat_rec.intldesg, "          ");
+
+        constexpr double XPDOTP = 1440.0 / (2 * PI);
+        sat_rec.no_kozai = sat_rec.no_kozai / XPDOTP;
+        sat_rec.ndot = sat_rec.ndot / (XPDOTP * 1440.0);
+        sat_rec.nddot = sat_rec.nddot / (XPDOTP * 1440.0 * 1440);
+        sat_rec.inclo = sat_rec.inclo * DEG_TO_RAD;
+        sat_rec.nodeo = sat_rec.nodeo * DEG_TO_RAD;
+        sat_rec.argpo = sat_rec.argpo * DEG_TO_RAD;
+        sat_rec.mo = sat_rec.mo * DEG_TO_RAD;
+
+        double startmfe = 0;
+        double stopmfe = 2880;
+        double deltamin = 120;
+        vallado_sgp4::sgp4init(
+            vallado_sgp4::wgs72, 'a', sat_rec.satnum, sat_rec.jdsatepoch + sat_rec.jdsatepochF - 2433281.5,
+            sat_rec.bstar, sat_rec.ndot, sat_rec.nddot, sat_rec.ecco, sat_rec.argpo, sat_rec.inclo,
+            sat_rec.mo, sat_rec.no_kozai, sat_rec.nodeo, sat_rec
+        );
+        auto sat = Satellite(sat_rec);
+
+        double tsince = startmfe;
+        while ((tsince < stopmfe) && sat.last_error() == Sgp4Error::NONE) {
+            tsince = std::min(tsince + deltamin, stopmfe);
+            CHECK(sat.propogate_from_epoch(tsince, pos, vel) == Sgp4Error::NONE);
+            std::fprintf(
+                out_file,
+                " %16.8f %16.8f %16.8f %16.8f %12.9f %12.9f %12.9f",
+                tsince, pos[0], pos[1], pos[2], vel[0], vel[1], vel[2]
+            );
+        }
+    }
+    std::fclose(out_file);
 }
