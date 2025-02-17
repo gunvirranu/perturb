972bd7fb45b4ea44b37a3fb01761f056d5c0c778
Gunvir Ranu <mail@gunvirranu.com>
2025-02-17 05:07:46 -0800

    Ref posq to pointer

--- src/sgp4.c
+++ src/sgp4.c
@@ -1324,7 +1324,7 @@ real_t * rp, real_t * rteosq, real_t * sinio, real_t * gsto, char opsmode
         *con42 = 1.0 - 5.0 * (*cosio2);
         *con41 = -(*con42) - (*cosio2) - (*cosio2);
         *ainv = 1.0 / (*ao);
-        posq = po * po;
+        *posq = po * po;
         rp = (*ao) * (1.0 - ecco);
         method = 'n';
 
@@ -1551,7 +1551,7 @@ real_t * rp, real_t * rteosq, real_t * sinio, real_t * gsto, char opsmode
         initl
             (satrec.xke, satrec.j2, satrec.ecco, epoch, satrec.inclo, satrec.no_kozai, satrec.operationmode,
             satrec.method, &ainv, &ao, satrec.con41, &con42, &cosio, &cosio2, eccsq, &omeosq,
-            posq, rp, &rteosq, &sinio, satrec.gsto, satrec.no_unkozai);
+            &posq, rp, &rteosq, &sinio, satrec.gsto, satrec.no_unkozai);
         satrec.a = pow(satrec.no_unkozai * satrec.tumin, (-2.0 / 3.0));
         satrec.alta = satrec.a * (1.0 + satrec.ecco) - 1.0;
         satrec.altp = satrec.a * (1.0 - satrec.ecco) - 1.0;
